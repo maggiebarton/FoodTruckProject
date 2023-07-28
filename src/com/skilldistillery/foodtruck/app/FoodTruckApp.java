b2aef4b80e58adf69f3a6877bcd390e5714e4e8d
Maggie Barton <maggiebarton2@gmail.com>
2023-07-28 17:04:41 -0600

    user can quit by entering quit for name

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -15,22 +15,28 @@ public class FoodTruckApp {
 	public void run() {
 		// get input from user
 		Scanner sc = new Scanner(System.in);
+		String name;
 
 		for (int i = 0; i < 5; i++) {
 			System.out.print("Enter the name of the Food Truck: ");
-			String name = sc.nextLine();
-			System.out.print("Enter the type of food : ");
-			String type = sc.nextLine();
-			System.out.print("Enter the rating on (1-5): ");
-			int rating = sc.nextInt();
-			sc.nextLine();
+			name = sc.nextLine();
+			if (name.equals("quit")) {
+				break;
+			} else {
 
-			// create food trucks and assign user input
-			FoodTruck truck = new FoodTruck();
-			truck.setName(name);
-			truck.setType(type);
-			truck.setRating(rating);
-			fleet[i] = truck;
+				System.out.print("Enter the type of food : ");
+				String type = sc.nextLine();
+				System.out.print("Enter the rating on (1-5): ");
+				int rating = sc.nextInt();
+				sc.nextLine();
+
+				// create food trucks and assign user input
+				FoodTruck truck = new FoodTruck();
+				truck.setName(name);
+				truck.setType(type);
+				truck.setRating(rating);
+				fleet[i] = truck;
+			}
 		}
 		sc.close();
 	}
